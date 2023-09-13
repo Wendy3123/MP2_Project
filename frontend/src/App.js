4d376b7b9bd59e38ad40f09a263f6bc208fc633f
Wendy Wang <wendywangg928@gmail.com>
2023-09-12 20:29:44 -0400

    cleaned up gitignore

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
 import "./App.css";
 import Home from "./Home.js";
 
+const backgroundAllPages = {
+  backgroundColor: "plum",
+};
 const headerStyle = {
-  fontSize: "45px",
+  fontSize: "52px",
   background: "linear-gradient(27deg, #7928CA, #FF0080)",
+  margin: "auto",
+  padding: "10px 0 10px 0",
 };
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={backgroundAllPages}>
       <Home headerStyle={headerStyle} />
     </div>
   );
