4758057989d54ebc296db77221cc3d15c42f1c28
Wendy Wang <wendywangg928@gmail.com>
2023-09-14 21:00:13 -0400

    adding form and usestate to form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,29 +2,34 @@ import "./App.css";
 import Form from "./pages/Form";
 import { Route, Routes } from "react-router-dom";
 import Post from "./pages/Post";
-// import { useState } from "react";
+import { useState } from "react";
 import Home from "./components/Home";
 import AboutUs from "./components/AboutUs";
 
+const initialState = {
+  author: "",
+  img_URL: "https://placekitten.com/300/300",
+  caption: "",
+};
+
 function App() {
+  const [form, setForm] = useState(initialState);
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/AboutUs" element={<AboutUs />} />
         <Route path="/AllPosts" element={<Post />} />
-        <Route path="/CreateAPost" element={<Form />} />
+        <Route
+          path="/CreateAPost"
+          element={<Form form={form} setForm={setForm} />}
+        />
       </Routes>
     </div>
   );
 }
 
 export default App;
-// const initialState = {
-//   author: "",
-//   img_URL: "https://placekitten.com/300/300",
-//   caption: "",
-// };
 
 // function App() {
 //   const [form, setForm] = useState(initialState);
