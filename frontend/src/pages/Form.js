a5f4e7f03f4c314856b69bbc56e697d3e99bb9cc
Wendy Wang <wendywangg928@gmail.com>
2023-09-19 21:33:15 -0400

    changed form styling

--- frontend/src/pages/Form.js
+++ frontend/src/pages/Form.js
@@ -26,38 +26,44 @@ function Form() {
     <div>
       <h1 className="h1Tag">
         <img src="./LOGO.png" alt="brand logo" className="logo"></img>
-        {isEdit ? "Edit A Post" : "Create A Post"}
+        TWITTER
       </h1>
       <NavBar />
-      <form onSubmit={savePostsToDatabase}>
-        {formFields.map((field) => (
-          <div className="form1">
-            <div>
-              <label className="form1label">{field.label}</label>
+      <div className="OutterFormBox">
+        <form onSubmit={savePostsToDatabase}>
+          <h1>
+            <img src="./LOGO.png" alt="brand logo" className="logo"></img>
+            {isEdit ? "Edit A Post" : "Create A Post"}
+          </h1>
+          {formFields.map((field) => (
+            <div className="form1">
+              <div>
+                <label className="form1label">{field.label}</label>
+              </div>
+              <div>
+                <input
+                  name={field.id}
+                  id={field.id}
+                  type={field.type}
+                  className="form1input"
+                  value={form[field.id]} //assign the value of the input now to the current form default value
+                  onChange={
+                    (e) => setForm({ ...form, [e.target.name]: e.target.value }) //...form takes the values we alrdy have in the form and updates the e.target.name to e.target.value
+                  }
+                ></input>
+              </div>
             </div>
-            <div>
-              <input
-                name={field.id}
-                id={field.id}
-                type={field.type}
-                className="form1input"
-                value={form[field.id]} //assign the value of the input now to the current form default value
-                onChange={
-                  (e) => setForm({ ...form, [e.target.name]: e.target.value }) //...form takes the values we alrdy have in the form and updates the e.target.name to e.target.value
-                }
-              ></input>
-            </div>
-          </div>
-        ))}
+          ))}
 
-        <div className="CreatePostBtn">
-          {/* change type to submit for button submit so it also renders the submit function when clicked */}
-          <button className="formbtn">
-            {isEdit ? "Add Edit" : "Create New Post"}
-          </button>
-          <button className="formbtncancel">Cancel</button>
-        </div>
-      </form>
+          <div className="CreatePostBtn">
+            {/* change type to submit for button submit so it also renders the submit function when clicked */}
+            <button className="formbtn">
+              {isEdit ? "Add Edit" : "Create New Post"}
+            </button>
+            <button className="formbtncancel">Cancel</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
